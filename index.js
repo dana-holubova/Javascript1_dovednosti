fa859419da3200ed26f3fb37191144fb48abd4a6
Dana Holubová <danmich@email.cz>
2023-11-19 11:14:24 +0100

    Dokončení úkolu.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('funguju!');
+const updateSkill = (idSkill, number) => {
+    document.querySelector(`#${idSkill} .skill__progress`).style.width = `${number}%`
+    document.querySelector(`#${idSkill} .skill__value`).textContent = `${number} / 100`
+}
+
+updateSkill('skill1', prompt('Zadejte vaši úroveň znalostí HTML na škále 1–100: '))
+updateSkill('skill2', prompt('Zadejte vaši úroveň znalostí CSS na škále 1–100: '))
+updateSkill('skill3', prompt('Zadejte vaši úroveň znalostí JavaScript na škále 1–100: '))
